f6099af8dcd54eaed27580ea97bb10c941932956
stan-2021131 <sergiocoromac@gmail.com>
2025-03-24 15:32:28 -0600

    Se agregó la clase Node para ser usada en el árbol binario

---
+++ Node.java
@@ -0,0 +1,24 @@
+
+public class Node<K extends Comparable<K>, V> implements Comparable<K>{
+    private K key;
+    private V value;
+
+    public Node(){
+    }
+
+    public Node(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {return key;}
+    public V getValue() {return value;}
+
+    public void setKey(K key) {this.key = key;}
+    public void setValue(V value) {this.value = value;}
+
+    @Override
+    public int compareTo(K otherKey) {
+        return this.key.compareTo(otherKey);
+    }
+}
