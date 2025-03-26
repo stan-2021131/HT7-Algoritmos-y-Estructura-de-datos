f97fe7b33247af398c830003ab0fe01ba23f6f98
stan-2021131 <sergiocoromac@gmail.com>
2025-03-26 17:31:41 -0600

    Se hicieron cambios lógicos en el método compareTo

--- Node.java
+++ Node.java
@@ -19,6 +19,9 @@ public class Node<K extends Comparable<K>, V> implements Comparable<K>{
 
     @Override
     public int compareTo(K otherKey) {
+        if (this.key instanceof String && otherKey instanceof String) {
+            return ((String)this.key).compareToIgnoreCase((String)otherKey);
+        }
         return this.key.compareTo(otherKey);
     }
 }
